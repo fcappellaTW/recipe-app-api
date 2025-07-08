34fcf48c800670ff65748c6ce50dbd54c21c4ed6
Fabrício Cappellari <fabricio.cappellari@thoughtworks.com>
2025-07-08 18:51:01 -0300

    Add tag model, routes and tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,20 +3,64 @@ Serializers for the recipe API view.
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for the tag object."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create and return a new tag."""
+        return Tag.objects.create(**validated_data)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for the recipe object."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def get_or_create(self, tags, recipe):
+        """Get or create tags as needed."""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
 
     def create(self, validated_data):
         """Create and return a new recipe."""
-        return Recipe.objects.create(**validated_data)
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self.get_or_create(tags, recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe, handling tags."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self.get_or_create(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
